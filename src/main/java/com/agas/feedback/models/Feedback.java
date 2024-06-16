dbf2819a219f8e7f5ea2a1498d2f99b000a95e70
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-06-16 18:25:30 +0300

    Cущность обратная связь

---
+++ src/main/java/com/agas/feedback/models/Feedback.java
@@ -0,0 +1,66 @@
+package com.agas.feedback.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Table(name = "feedback")
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    //@JsonIgnore
+    private Long Id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Min(7)
+    private String tel;
+
+    @NotNull
+    @Min(10)
+    private String feedback;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getFeedback() { return feedback; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
