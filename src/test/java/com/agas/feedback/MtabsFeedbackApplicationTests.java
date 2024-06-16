ebba0ed5a2049e7d883864f9d0df661d98e620bd
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-06-16 14:13:16 +0300

    Начало работы над сервисом feedback

---
+++ src/test/java/com/agas/feedback/MtabsFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agas.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtabsFeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
