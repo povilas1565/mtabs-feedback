0b6b4c0142e03ef1c98b84503e3599c7901e59c1
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-06-17 12:35:31 +0300

    Commit Changes

--- src/main/java/com/agas/feedback/FeedbackApplication.java
+++ src/main/java/com/agas/feedback/FeedbackApplication.java
@@ -2,12 +2,15 @@ package com.agas.feedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@EntityScan("com.aga.feedback.model")
+@SpringBootApplication(scanBasePackages = {"com.agas.feedback.models", "com.agas.feedback.services", "com.agas.feedback.controllers", "com.agas.feedback.configuration"})
 public class FeedbackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeedbackApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(FeedbackApplication.class, args);
     }
 
 }
