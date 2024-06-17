8775e0525bd6f9482aabdae316d1dcf43d8fd6d5
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-06-17 12:36:22 +0300

    API для формы обратной связи

---
+++ src/main/java/com/agas/feedback/controllers/FeedbackController.java
@@ -0,0 +1,50 @@
+package com.agas.feedback.controllers;
+
+import com.agas.feedback.models.Feedback;
+import com.agas.feedback.services.EmailConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ValidationException;
+
+@RestController
+@Slf4j
+@RequestMapping("/")
+public class FeedbackController {
+
+    @Autowired
+    private EmailConfig emailConfig;
+
+    @PostMapping(value = "/feedback", produces = "application/json;charset=UTF-8")
+    public void sendFeedback(@RequestBody Feedback feedback, BindingResult bindingResult) throws ValidationException {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Feedback is not valid");
+        }
+
+        // Create mail sender
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(this.emailConfig.getHost());
+        mailSender.setPort(this.emailConfig.getPort());
+        mailSender.setUsername(this.emailConfig.getUsername());
+        mailSender.setPassword(this.emailConfig.getPassword());
+
+        // Create mailform instance
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(feedback.getEmail());
+        mailMessage.setTo("agoramas.net@gmail.com");
+        mailMessage.setSubject("New feedback from " + feedback.getName() + feedback.getTel());
+        mailMessage.setText(feedback.getFeedback());
+
+        // Send mail
+        mailSender.send(mailMessage);
+    }
+}
